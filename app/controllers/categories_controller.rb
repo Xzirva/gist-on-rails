a6d0d591e1956ba584bc6492c457c81aeed54a14
Yoann SOH <yoannludwigsoh@yahoo.fr>
2016-04-03 18:59:01 +0200

    Fixing destroy category action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -80,6 +80,8 @@ class CategoriesController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_category
+    render json: params
+    return
     @category = Category.find(params[:id])
   end
 
